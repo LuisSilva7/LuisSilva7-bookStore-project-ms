1d9ea3ff6b353a77aa1ad015eb709c70aae6126c
LuisSilva7 <lmpsilva7@gmail.com>
2025-05-27 23:01:32 +0100

    created order-query service

---
+++ bookStore-backend/services/order-query/src/test/java/org/bookStore/order_query/OrderQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bookStore.order_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
